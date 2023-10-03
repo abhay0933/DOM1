29a8350596d475da1aaf1b3f228b0763e03bf3fd
abhay0933 <abhay80912@gmail.com>
2023-10-03 19:38:04 +0530

    DOM assignmet done

---
+++ script.js
@@ -0,0 +1,8 @@
+let a = document.getElementsByClassName("box");
+a[0].style.color = "blue";
+a[0].style.backgroundColor = "black";
+a[0].style.width = "35%";
+a[0].style.height = "15rem";
+a[0].style.padding = "10px";
+a[0].style.paddingLeft = "20px";
+a[0].style.margin = "20px";
